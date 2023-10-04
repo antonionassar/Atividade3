fada7442b81e22556084e5cc5395837fc36e47c4
Antonio Nassar <99207613+antonionassar@users.noreply.github.com>
2023-10-04 11:09:06 -0300

    Update codigo1.ino
    Adição de um laço 'for' + alterações no código

--- SEMANA_01/01_CODIGOS/codigo1/codigo1/codigo1.ino
+++ SEMANA_01/01_CODIGOS/codigo1/codigo1/codigo1.ino
@@ -5,58 +5,82 @@ String textoUsuario;
 int numeroUsuario;
 float numeroFlutuanteUsuario;
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial) {
-    delay(10);
-  }
+void setup()
+{
+    Serial.begin(9600);
+    while (!Serial)
+    {
+        delay(10);
+    }
 }
 
-void loop() {
-  if (usuarioDesejaContinuar) {
-    Serial.println("Digite um texto:");
-    while (Serial.available() == 0) {
-      delay(10);
-    }
-    textoUsuario = Serial.readString();
+void loop()
+{
+    if (usuarioDesejaContinuar)
+    {
+        Serial.println("Digite um texto:");
+        while (Serial.available() == 0)
+        {
+            delay(10);
+        }
+        textoUsuario = Serial.readString();
 
-    Serial.println("Digite um número inteiro:");
-    while (Serial.available() == 0) {
-      delay(10);
-    }
-    numeroUsuario = Serial.parseInt();
+        Serial.println("Digite um número inteiro:");
+        while (Serial.available() == 0)
+        {
+            delay(10);
+        }
+        numeroUsuario = Serial.parseInt();
 
-    Serial.println("Digite um número flutuante:");
-    while (Serial.available() == 0) {
-      delay(10);
-    }
-    numeroFlutuanteUsuario = Serial.parseFloat();
+        Serial.println("Digite um número flutuante:");
+        while (Serial.available() == 0)
+        {
+            delay(10);
+        }
+        numeroFlutuanteUsuario = Serial.parseFloat();
 
-    if (numeroUsuario > 10) {
-      Serial.println("O número inteiro é maior que 10.");
+        if (numeroUsuario > 10)
+        {
+            Serial.println("O número inteiro é maior que 10.");
 
-      if (numeroFlutuanteUsuario > 20.0) {
-        Serial.println("O número flutuante é maior que 20.0.");
-      }
-    } else {
-      Serial.println("O número inteiro é menor ou igual a 10.");
-    }
+            if (numeroFlutuanteUsuario > 20.0)
+            {
+                Serial.println("O número flutuante é maior que 20.0.");
+            }
+        }
+        else
+        {
+            Serial.println("O número inteiro é menor ou igual a 10.");
+        }
 
-    if (textoUsuario.length() >= 5) {
-      Serial.println("O texto inserido tem 5 ou mais caracteres.");
-    } else {
-      Serial.println("O texto inserido tem menos de 5 caracteres.");
-    }
+        if (textoUsuario.length() >= 5)
+        {
+            Serial.println("O texto inserido tem 5 ou mais caracteres.");
+        }
+        else
+        {
+            Serial.println("O texto inserido tem menos de 5 caracteres.");
+        }
 
-    Serial.println("Digite 1 para continuar ou 0 para encerrar.");
-    while (Serial.available() == 0) {
-      delay(10);
+        Serial.println("Imprimindo os primeiros " + String(numeroUsuario) + " números inteiros:");
+        for (int i = 1; i <= numeroUsuario; i++)
+        {
+            Serial.println(i);
+        }
+
+        Serial.println("Digite 1 para continuar ou 0 para encerrar.");
+        while (Serial.available() == 0)
+        {
+            delay(10);
+        }
+        usuarioDesejaContinuar = static_cast<bool>(Serial.parseInt());
     }
-    usuarioDesejaContinuar = static_cast<bool>(Serial.parseInt());
-  } else {
-    Serial.println("Programa encerrado.");
-    while (true) {
-      delay(10);
+    else
+    {
+        Serial.println("Programa encerrado.");
+        while (true)
+        {
+            delay(10);
+        }
     }
-  }
 }
